ec39276b5dcc4ae74ee00776916acc9df74c02f1
ProgrammerDev <65603725+ProgrammerDev@users.noreply.github.com>
2020-07-01 10:39:45 +0200

    Weitere Beispiele hinzugefügt

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
 #Discord Importen
 import discord
 
+#Verinfachung: Client definieren
+
+client = discord.Client()
+
+# Event
+
+@client.event
+#Wenn der Bot Ready ist
+async def on_ready():
+    #Etwas ausgeben
+    print("Gestartet")
+
 #Client starten
-discord.Client().run("token")
+client.run("token")
